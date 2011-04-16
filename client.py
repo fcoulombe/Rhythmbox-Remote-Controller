4832a886b3c254d3826d0c5856997be96145aaaa
Francois Coulombe <fcoulombe@silentfalls.org>
2011-04-16 12:57:46 -0700

    franky adds the base for the client and server

---
+++ client.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# sigslot.py
+
+import sys
+import socket
+from PyQt4 import QtGui, QtCore
+
+host = 'localhost' 
+port = 50000 
+size = 1024 
+
+def sendCommand(command):    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.connect((host,port)) 
+    s.send(command) 
+    data = s.recv(size) 
+    s.close() 
+    return data
+    #print 'Received:', data
+
+
+
+class RythmBoxClient(QtGui.QWidget):
+  
+    def __init__(self):
+        super(RythmBoxClient, self).__init__()
+        
+        self.initUI()
+
+    def updateTrackName(self):
+        self.songPlaying.setText("currently playing: \n" + songPlaying)
+
+    def next(self):
+        print "next"
+        songPlaying = sendCommand("Next")
+        self.updateTrackName(songPlaying)
+
+    def prev(self):
+        print "prev"
+        songPlaying = sendCommand("Prev")
+        self.updateTrackName(songPlaying)
+
+    def play(self):
+        print "play/pause"
+        songPlaying = sendCommand("Play")
+        self.updateTrackName(songPlaying)
+
+    def reload(self):
+        print "reload"
+        songPlaying = sendCommand("Reload")
+        self.updateTrackName(songPlaying)
+
+
+    def initUI(self):
+
+	self.serverAddress = QTextInput(self)
+        self.songPlaying = QtGui.QLabel(self)
+        
+        self.b1 = QtGui.QPushButton("Next", self)
+        self.b2 = QtGui.QPushButton("Prev", self)
+        self.b3 = QtGui.QPushButton("Play/Pause", self)
+        #self.b5 = QtGui.QPushButton("ReloadPlayList", self)
+        #self.playlist = QtGui.QListWidget(self)
+
+        self.vbox = QtGui.QVBoxLayout()
+    
+        self.vbox.addWidget(self.songPlaying)
+        self.vbox.addWidget(self.b2)
+        self.vbox.addWidget(self.b3)
+        self.vbox.addWidget(self.b1)
+
+        self.setLayout(self.vbox)
+        self.connect(self.b1,  QtCore.SIGNAL('clicked()'), self.next)
+        self.connect(self.b2,  QtCore.SIGNAL('clicked()'), self.prev)
+        self.connect(self.b3,  QtCore.SIGNAL('clicked()'), self.play)
+
+
+        self.setWindowTitle('Rythmbox Remote')
+        self.resize(250, 150)
+
+
+app = QtGui.QApplication(sys.argv)
+ex = RythmBoxClient()
+ex.show()
+sys.exit(app.exec_())
---
+++ server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python 
+
+import socket 
+import subprocess
+
+def runProgram(program, command):
+    p = subprocess.Popen(program + " " + command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    lines = p.stdout.readlines()
+    for line in lines:
+        print line,
+    retval = p.wait()
+    return lines
+
+def execCommand(command):
+    from subprocess import call
+    ret = runProgram("rhythmbox-client", command)
+    return str(runProgram("rhythmbox-client", "--print-playing"))
+    
+
+host = '' 
+port = 50000 
+backlog = 5 
+size = 1024 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind((host,port)) 
+s.listen(backlog) 
+while 1: 
+    client, address = s.accept() 
+    data = client.recv(size) 
+    if data == None:
+        continue
+
+    if data == "Next":
+        #exec next command
+        ret = execCommand("--next")
+        client.send(ret)
+    if data == "Prev":
+        print "exec prev"
+        ret = execCommand("--previous")
+        client.send(ret)
+
+    if data == "Play":
+        print "exec play"
+        ret = execCommand("--play-pause")
+        client.send(ret)
+
+    if data == "GetList":
+        print "send playlist to client"
+        ret = execCommand("--next")
+        client.send(ret)
+
+    if data == "Jump": #play a specific song
+        #obtain song name
+        print "exec jump command"
+       
+    if data == "enqueue":
+        print "exec enqueue command" 
+    client.close()
